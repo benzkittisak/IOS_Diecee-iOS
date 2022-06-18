587a76b644b22e5ee3af17169f167a7762b9ed21
benzkittisak <benzkittisak@stv.ac.th>
2022-06-18 11:16:56 +0700

    update code

--- Diecee-iOS/ViewController.swift
+++ Diecee-iOS/ViewController.swift
@@ -8,17 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var diceIV1: UIImageView!
     @IBOutlet weak var diceIV2: UIImageView!
     
-    @IBOutlet weak var rollButton: UIButton!
     
+    
+    //    เมื่อ Application มันโหลดขึ้นมาจะให้ทำอะไร เหมือน onCreate ใน Android แหละ
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //        สั่งให้เปลี่ยนรูปภาพใน Xcode 13 เราจะใช้ #imageLiteral() แบบนอ้แล้วก็เลือกรูปเลย
+        diceIV1.image = #imageLiteral(resourceName: "DiceSix")
+        //        ปรับค่า aplha ของ element ก็ทำได้นา
+        //        diceIV1.alpha = 0.5
+        diceIV2.image = #imageLiteral(resourceName: "DiceTwo")
+               
     }
-
-
+    
+    @IBAction func rollBtn(_ sender: UIButton) {
+        print("Button got tapped.")
+        diceIV1.image = #imageLiteral(resourceName: "DiceFour")
+        diceIV2.image = #imageLiteral(resourceName: "DiceFour")
+    }
 }
 
